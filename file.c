da90899ba14dbe6d910d11936f98a7ae9168fd02
gazizkv <valitovgaziz@gmail.com>
2020-09-06 21:29:10 +0500

    1st

---
+++ file.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	FILE * fpointer = fopen("employees.txt", "a");
+
+	fprintf(fpointer, "\nKelly, Customer Service");
+
+	fclose(fpointer);
+
+	return 0;
+}
