e7b9f39b084372642f27ae852465182ace5b9ff4
gazizkv <valitovgaziz@gmail.com>
2020-12-14 15:25:28 +0500

    main file calculator

---
+++ kalkulator/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	double first_digit, second_digit
+	printf("Please enter the first digit");
+	scanf("", first_digit);
+	printf("Please enter the secind digit");
+	scanf("%", secind_digit);
+	return 0;
+}
