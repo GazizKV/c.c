1723e0d9c3ac27c55cb576dfffebad71d9e799df
gazizkv <valitovgaziz@gmail.com>
2020-12-14 20:41:01 +0500

    func purt added

--- kalkulator/main.c
+++ kalkulator/main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char* argv[])
 {
-	double first_digit, second_digit
-	printf("Please enter the first digit");
-	scanf("", first_digit);
-	printf("Please enter the secind digit");
-	scanf("%", secind_digit);
+	float first_digit, second_digit, result;
+	char* sign;
+	printf("Please enter the first digit\n");
+	scanf("%f", &first_digit);
+	printf("Please enter the secind digit\n");
+	scanf("%f", &second_digit);
+	printf("Please enter + or - or * or /\n");
+	scanf("%s", sign);
+	printf("%f %f %s", first_digit, second_digit, sign);
+	if ("+" == sign)
+		result = first_digit + second_digit;
+	if ("-" == sign)
+		result = first_digit - second_digit;
+	if ("*" == sign)
+		result = first_digit * second_digit;
+	if ("/" == sign)
+		result = first_digit / second_digit;
+	printf("%f %s %f = %f\n", first_digit, sign ,second_digit, result);
 	return 0;
 }
