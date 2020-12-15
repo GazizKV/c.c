3552a3de912f85097e0677c6a9bed920f9f4101f
gazizkv <valitovgaziz@gmail.com>
2020-12-15 15:07:53 +0500

    ended the kalkulator

--- kalkulator/main.c
+++ kalkulator/main.c
@@ -3,6 +3,10 @@
 
 int main(int argc, char* argv[])
 {
+	char* plus = "+";
+	char* minus = "-";
+	char* didgital = "*";
+	char* divid = "/";
 	float first_digit, second_digit, result;
 	char* sign;
 	printf("Please enter the first digit\n");
@@ -11,15 +15,27 @@ int main(int argc, char* argv[])
 	scanf("%f", &second_digit);
 	printf("Please enter + or - or * or /\n");
 	scanf("%s", sign);
-	printf("%f %f %s", first_digit, second_digit, sign);
-	if ("+" == sign)
-		result = first_digit + second_digit;
-	if ("-" == sign)
-		result = first_digit - second_digit;
-	if ("*" == sign)
-		result = first_digit * second_digit;
-	if ("/" == sign)
-		result = first_digit / second_digit;
+	printf("%f %f %s\n", first_digit, second_digit, sign);
+	if (*plus == *sign)
+	{
+		result = first_digit+second_digit;
+		printf("check the station plus %f\n", result);
+	}
+	if (*minus == *sign)
+	{
+		result = first_digit-second_digit;
+		printf("check the station minus %f\n", result);
+	}
+	if (*didgital == *sign)
+	{
+		result = first_digit*second_digit;
+		printf("check the station times %f\n", result);
+	}
+	if (*divid == *sign)
+	{
+		result = first_digit/second_digit;
+		printf("check the station divide %f\n", result);
+	}
 	printf("%f %s %f = %f\n", first_digit, sign ,second_digit, result);
 	return 0;
 }
