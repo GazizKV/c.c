b83f3a5a42d649f7bb566d6109b9e9b10cc13a5f
gazizkv <valitovgaziz@gmail.com>
2020-12-22 17:05:13 +0500

    fopen and scanf?

--- sudoku_solver/main.c
+++ sudoku_solver/main.c
@@ -1,5 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char* argv[]){
-	char ** sudoku = fscanf(sudoku);
+	FILE * f;
+	f=fopen("sudoku","r");
+	char ** sudo;
+
+	for(int i=0;i<9;i++)
+		for(int j=0;j<0;j++)
+		{
+			fscanf(f, "%s", &sudo[i][j]);
+			printf("%s", &sudo[i][j]);
+		}
+	for(int i=0;i<9;i++)
+		for(int j=0;j<0;j++)
+			printf("%s", &sudo[i][j]);
+	
 
 	
 
