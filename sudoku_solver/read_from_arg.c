4d321c460837c81edeeb0008de15f0c9f4cc0001
gazizkv <valitovgaziz@gmail.com>
2020-12-31 20:52:06 +0500

    need too meny gixessssss

--- sudoku_solver/read_from_arg.c
+++ sudoku_solver/read_from_arg.c
@@ -2,35 +2,34 @@
 #include <string.h>
 #include <stdlib.h>
 
-int check(int argc, char **argv)
+int check(int argc)
 {
 	if (argc < 2)
 	{
 		printf("You are not pass the argument");
 		return 1;
 	}
-	if ((*argv[0] != "\"" && *argv[10])!="\"")
+	/*if ((*argv[0] != "\"" && *argv[10])!="\"")
 	{
 		printf("You are pass the argument in a wrong way");
 		return 1;
-	}
+	}*/
 }
 
 void printing(char **argv)
 {
-	printf(strlen(argv);
-	for (int i=0; i < strlen(argv); i++)
+	for (int i=0; i < strlen(*argv[1]); i++)
 	{
-		printf(*argv[i]);
+		putchar(argv[i]);
 	}
 }
 
-int main(int argc, char* argv[]){
-	printing(argv);
-	if (check(argc, argv)) return 0;
+int main(int argc, char* argv){
+	if (check(argc)) return 0;
+	printing(*argv);
 	int ** sudoku;
 
-	printf("%s", *argv);
+	printf("%s", argv);
 	
 
 	return 0;
