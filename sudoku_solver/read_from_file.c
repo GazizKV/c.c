d285a2cb2e76984c48ac47bc5f5f061e9fc619b0
gazizkv <valitovgaziz@gmail.com>
2021-01-02 19:14:10 +0500

    cant read from file lear and read more

--- sudoku_solver/read_from_file.c
+++ sudoku_solver/read_from_file.c
@@ -3,28 +3,26 @@
 
 int main(int argc, char* argv[]){
 	FILE *fsudoku;
-	char numbers;
-	int characters;
-	
-	characters = 0;
+	char *sudoku;
+	int i;
 
+	printf("16ths line");
 
 	fsudoku=fopen("sudoku","r");
+
+	printf("16ths line");
 
 	if (fsudoku == 0) {
 		printf("Cannot open file\n");
 		exit(1);
 	}
 
-	
-	while (fgetc(fsudoku) != EOF) {
-		numbers = getc(fsudoku);
-		if (numbers == '-') characters++;
-		putchar(numbers);
-	}
-
+	printf("16ths line");
+	i = 0;
+	fgets(sudoku, 10, fsudoku);
+	printf("%s", sudoku);
+	printf("\n");
 	fclose(fsudoku);
-	printf(" Numbers of \"-\" characters = %d", characters);
 	
 
 
